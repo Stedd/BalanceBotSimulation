a96d5f60728f5b34830be8097b8200de58a2d211
Stedd <toanpa+git@pm.me>
2021-10-30 02:09:26 +0200

    Updates

--- Assets/drive.cs
+++ Assets/drive.cs
@@ -2,53 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class drive : MonoBehaviour
+public class Drive : MonoBehaviour
 {
-    ArticulationBody _drive;
-    ArticulationDrive _artDrive;
-        
-    [SerializeField]GameObject _body;
+    [Header("Drives")]
+    [SerializeField] ArticulationBody _leftWheelDrive;
+    [SerializeField] ArticulationBody _rightWheelDrive;
+
+    [Header("Body")]
+    [SerializeField] GameObject _body;
+    [SerializeField] float angle;
+    [SerializeField] Vector3 speedVec;
+    [SerializeField] float speed;
+
+    [Header("LeftWheel")]
+    [SerializeField] float _lwheelAngle = 0;
+    [SerializeField] float _langleSp = 0;
+    [SerializeField] float _ldriveCommand;
+    //[SerializeField] float _lError;
+    //[SerializeField] float _lIerror;
+    [Header("RightWheel")]
+    [SerializeField] float _rwheelAngle = 0;
+    [SerializeField] float _rangleSp = 0;
+    [SerializeField] float _rdriveCommand;
+    //[SerializeField] float _rError;
+    //[SerializeField] float _rIerror;
 
 
-    [SerializeField]float wheelAngle = 0;
-    [SerializeField]float angle;
-    [SerializeField]float angleSp = 0;
+    [Header("Speed Controller")]
+    [SerializeField] float speedSp;
+    [SerializeField] float speedGain = 0.1f;
+    [SerializeField] float speedError;
+    [SerializeField] float speedAngleCorrection;
 
-    [SerializeField]float driveCommand ; 
+    [Header("Angle Controller")]
+    [SerializeField] float angleSp;
 
-    
-    [SerializeField]float pGain = 10f;
-    [SerializeField]float error;
+    [SerializeField] float pGain = 10f;
+    [SerializeField] float error;
 
-    [SerializeField]float iGain = 0.1f;
-    [SerializeField]float ierror;
+    [SerializeField] float iGain = 0.1f;
+    [SerializeField] float ierror;
 
+    #region Private
+    private ArticulationDrive _artDrive;
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        // _body = GetComponentInParent<GameObject>();
-        _drive = GetComponent<ArticulationBody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        speedVec = _leftWheelDrive.GetComponent<ArticulationBody>().velocity+ _rightWheelDrive.GetComponent<ArticulationBody>().velocity/2;
+        speedVec.y = 0;
+        speed = Vector3.Magnitude(speedVec);
+
+        speedError = (speedSp - speed);
+
+        speedAngleCorrection = (speedError * speedGain)/100;
+
         angle = _body.transform.rotation.x;
 
-        error = (angleSp - angle);
+        error = (angleSp - angle) + speedAngleCorrection;
 
         ierror += error * Time.deltaTime;
 
-        wheelAngle += (error*pGain) + (ierror* iGain);
-        SetDriveValues();
+        _lwheelAngle += (error * pGain) + (ierror * iGain);
+        SetDriveValues(_leftWheelDrive, _lwheelAngle);
+
+        _rwheelAngle += (error * pGain) + (ierror * iGain);
+        SetDriveValues(_rightWheelDrive, _rwheelAngle);
     }
 
-    private void SetDriveValues()
+    private void SetDriveValues(ArticulationBody _artB, float _wheelTargetAngle)
     {
-        _artDrive = _drive.xDrive;
-        _artDrive.target = -wheelAngle;
+        _artDrive = _artB.xDrive;
+        _artDrive.target = -_wheelTargetAngle;
         // _artDrive.targetVelocity = 5f;
-        _drive.xDrive = _artDrive;
+        _artB.xDrive = _artDrive;
     }
+
+
 }
+
